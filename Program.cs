e85931375db871b67e4e5c02be5bdfcf298b08b6
Deborah Novais <61918962+deborahnovais@users.noreply.github.com>
2020-11-13 09:31:28 -0300

    Programa concluído

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Condicionais_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade >= 18) {
+                Console.WriteLine("Você gostaria participar do time de CS GO? - SIM ou NÃO");
+                string resposta = Console.ReadLine();
+                if(resposta == "SIM") {
+                    Console.WriteLine("Compareça à Secretaria e fale com a Sara");
+                }
+                else {
+                    Console.WriteLine("Beleza. Passar bem!");
+                }
+            }
+
+            else {
+                Console.WriteLine("Você gostaria de participar do time de LoL? - SIM ou NÃO");
+                string resposta = Console.ReadLine();
+                if(resposta == "SIM") {
+                    Console.WriteLine("Compareça à Secretaria e fale com o Juscelino");
+                }
+                else {
+                    Console.WriteLine("Muito obrigado!");
+                }
+            }
+        }
+    }
+}
